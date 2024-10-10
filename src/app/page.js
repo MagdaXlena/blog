44c2bc9f48cc2c24e020a501429cac84f835f957
MAGDALENA KAPUSCINSKA <MADZIAA82@HOTMAIL.COM>
2024-10-10 18:53:55 +0100

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
-       <h2>Discover the thrill of cycling with our latest reviews, tips, and bike gear recommendations for every type of rider.</h2>
+
     </div>
   )
 }
