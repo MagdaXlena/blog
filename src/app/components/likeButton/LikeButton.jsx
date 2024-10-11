48bdee4e30a8cc9fe9036a26803f13672ce75d20
MAGDALENA KAPUSCINSKA <MADZIAA82@HOTMAIL.COM>
2024-10-11 07:54:55 +0100

    button

--- src/app/components/likeButton/LikeButton.jsx
+++ src/app/components/likeButton/LikeButton.jsx
@@ -1,15 +1,66 @@
 "use client"
 
-import { useState } from "react"
-import styles from "./likeButton.module.css"
 
-export default function LikeButton() {
+import React, { useState } from 'react';
 
-    const [count, setCount] = useState(0)
-    return(
-        <div className={styles.likeButton}>
-            <p>{count}</p>
-            <button className={styles.like} onClick={() => setCount(count + 1)}>Likes</button>
-        </div>
-    )
-}
+const LikeButton = () => {
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    if (isLiked) {
+      setLikes(likes - 1);
+      setIsLiked(false);
+    } else {
+      setLikes(likes + 1);
+      setIsLiked(true);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleLike}>
+        {isLiked ? '❤️ Liked' : '🤍 Like'}
+      </button>
+    </div>
+  );
+};
+
+export default LikeButton;
+
+// import { useState, useEffect} from "react"
+
+// import styles from "./likeButton.module.css"
+
+// export default function LikeButton() {
+
+//     const [likes, setLikes] = useState(0);
+
+//     useEffect(() => {
+//         const storedLikes = localStorage.getItem('likes');
+//         if (storedLikes) {
+//             setLikes(parseInt(storedLikes));
+//         }
+//     }, []);
+
+//     useEffect(() => {
+//         localStorage.setItem('likes', likes);
+//     }, [likes]);
+
+//     const handleLike = () => {
+//         setLikes(likes + 1);
+//     };
+
+//     return(
+//         <div>
+//             <button className={styles.likeButton} onClick={handleLike}>
+//             <p>{likes} {likes ===1 ? 'like' : 'likes'}</p>
+//             </button>
+//         </div>
+//     )
+// }
+
+    // const [count, setCount] = useState(0)
+
+            {/* <p>{count}</p>
+            <button className={styles.like} onClick={() => setCount(count + 1)}>Likes</button> */}
